b98b2797bfb6b742391b76fab9374e2845bfa3da
Josh Soref <2119212+jsoref@users.noreply.github.com>
2024-05-30 23:29:06 -0400

    spelling: finalize
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- deploy-board/deploy_board/static/editable/js/bootstrap-editable.js
+++ deploy-board/deploy_board/static/editable/js/bootstrap-editable.js
@@ -1570,7 +1570,7 @@ Makes editable any HTML element on the page. Applied as jQuery method.
                doAutotext = false;
             }
 
-            //depending on autotext run render() or just finilize init
+            //depending on autotext run render() or just finalize init
             $.when(doAutotext ? this.render() : true).then($.proxy(function() {
                 if(this.options.disabled) {
                     this.disable();
